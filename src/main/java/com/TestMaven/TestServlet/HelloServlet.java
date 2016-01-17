169b79edde40556e25204c63fb778742e220342a
luohao <l_how@live.com>
2016-01-17 21:59:50 +0800

    Maven的Web项目的基本配置

---
+++ src/main/java/com/TestMaven/TestServlet/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.TestMaven.TestServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by summer on 16-1-17.
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request,response);
+    }
+    public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+        DateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime=dateFormat.format(new Date());
+        request.setAttribute("currentTime",currentTime);
+        request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request,response);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: summer
+  Date: 16-1-17
+  Time: 下午9:00
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>TestHello</title>
+</head>
+<body>
+    <h1>当前时间：${currentTime}</h1>
+</body>
+</html>
